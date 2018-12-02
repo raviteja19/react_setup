7070ee48f1bd86c3d2f931a26dae66550e31d3b9
ravittho <ravittho@RAVITTHO-IN.oradev.oraclecorp.com>
2018-12-02 17:21:08 +0530

    commit 3

--- webcontent/js/main/Reducers/rootReducer.js
+++ webcontent/js/main/Reducers/rootReducer.js
@@ -3,12 +3,10 @@ import Login from './loginReducer';
 import Register from './registerReducer';
 import {CheckUserId} from './checkReducer'
 import ForgotPassword from './forgotPassword'
-import {getMoviesName} from './MoviesReducer'
-import {getMoviesDetails} from './MoviesReducer'
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
-  Login,Register,CheckUserId,ForgotPassword,getMoviesName,getMoviesDetails,routing: routerReducer
+  Login,Register,CheckUserId,ForgotPassword,routing: routerReducer
 });
 
 export default rootReducer;
--- webcontent/js/main/index.js
+++ webcontent/js/main/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './jsx/Main.jsx';
 import App from './jsx/App.jsx';
 import {BrowserRouter as Router, browserHistory} from 'react-router-dom';
 import routes from './routes';
